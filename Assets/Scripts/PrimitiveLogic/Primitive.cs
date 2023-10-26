4546fcddebfeae704cba27fef7e94dd6f751c66b
Vanchegs <manshin9300@gmail.com>
2023-10-26 23:08:45 +0300

    minFix

--- Assets/Scripts/PrimitiveLogic/Primitive.cs
+++ Assets/Scripts/PrimitiveLogic/Primitive.cs
@@ -5,6 +5,8 @@ namespace Vanchegs.PrimitiveLogic
 {
     public class Primitive : MonoBehaviour
     {
+        private const string FloorTag = "Floor";
+        
         [SerializeField] private Rigidbody2D primRigidbody2D;
 
         private bool isFloorTouch = false;
@@ -29,8 +31,8 @@ namespace Vanchegs.PrimitiveLogic
         
         private void OnCollisionEnter2D(Collision2D collision2D)
         {
-            if (isFloorTouch != false) return;
-            if (!collision2D.gameObject.CompareTag($"Floor")) return;
+            if (isFloorTouch) return;
+            if (!collision2D.gameObject.CompareTag(FloorTag)) return;
             primRigidbody2D.AddForce(new Vector2(Random.Range(-40f, 40f), Random.Range(5f, 7f)));
             isFloorTouch = true;
         }
