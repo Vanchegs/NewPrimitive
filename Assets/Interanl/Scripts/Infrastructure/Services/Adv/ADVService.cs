16dc39fc78a09d6ad26e7f3bc6f53c01931dbeae
Rimuru <Rimuru.Dev@gmail.com>
2023-11-04 23:40:06 +0400

    Added ADVService

---
+++ Assets/Interanl/Scripts/Infrastructure/Services/Adv/ADVService.cs
@@ -0,0 +1,29 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+using YG;
+
+namespace Vanchegs.Interanl.Scripts.Infrastructure.Services.Adv
+{
+    public sealed class ADVService : IADVService
+    {
+        public void ShowFullScreenADV()
+        {
+            if (IsSDKNotInit())
+                return;
+
+            YandexGame.FullscreenShow();
+        }
+
+        private static bool IsSDKNotInit() =>
+            !YandexGame.SDKEnabled;
+    }
+}
