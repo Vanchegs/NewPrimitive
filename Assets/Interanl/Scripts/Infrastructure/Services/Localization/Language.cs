47a63ad24f30622c58dea7f6418bf7e1522b759f
Rimuru <Rimuru.Dev@gmail.com>
2023-11-04 23:40:14 +0400

    Added localization

---
+++ Assets/Interanl/Scripts/Infrastructure/Services/Localization/Language.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Vanchegs.Interanl.Scripts.Infrastructure.Services.Localization
+{
+    [Serializable]
+    public enum Language
+    {
+        Ru = 0,
+        En = 1,
+    }
+}
